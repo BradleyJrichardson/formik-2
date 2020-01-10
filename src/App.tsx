89a1fc734935cc302d4c626d205fd7b9ff83e065
BradleyJrichardson <brdl.richardson@gmail.com>
2020-01-10 13:29:00 +1100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
-import {  } from "formik";
+import React from "react";
+import { Formik } from "formik";
+import { TextField } from "@material-ui/core";
+interface AppProps {}
 
+const App: React.FC<AppProps> = ({}) => {
+  return (
+    <>
+      <Formik
+        initialValues={{ firstName: "brad" }}
+        onSubmit={data => {
+          console.log("data", data);
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <TextField
+              name="firstName"
+              value={values.firstName}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            <pre>{JSON.stringify(values, null, 2)}</pre>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
 
-interface AppProps {
-
-}
-
-export const App: React.FC<AppProps> = ({}) => {
-    return ();
-}
+export default App;
